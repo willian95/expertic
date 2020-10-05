da078b142be27e14465108904e5253ab7bec481c
Victor Moreno <vamzfe@gmail.com>
2020-10-05 11:29:38 -0400

    Se agregaron campos al modal de instituciones

--- resources/views/admin/business/index.blade.php
+++ resources/views/admin/business/index.blade.php
@@ -7,21 +7,77 @@
         <div class="custom-modal-cover" v-show="modal">
 
             <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-6 offset-lg-3 col-md-12">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-lg-10 col-md-10">
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title">Instituciones</h5>
                             </div>
                             <div class="modal-body">
-                                <div class="form-group">
-                                    <label for="name">Nombre</label>
-                                    <input type="text" class="form-control" id="name">
+                            <div class="row justify-content-center">
+
+                                <div class="col-12 col-md-6 col-lg-6">
+                                   <div class="form-group">
+                                      <label for="name">Rut</label>
+                                      <input type="text" class="form-control" id="name">
+                                   </div>
                                 </div>
-                                <div class="form-group">
+
+
+                                <div class="col-12 col-md-6 col-lg-6">
+                                  <div class="form-group">
                                     <label for="logo">Logo</label>
                                     <input type="file" class="form-control" id="logo">
+                                  </div>
+                                </div>  
+
+                                <div class="col-12 col-md-6 col-lg-6">
+                                  <div class="form-group">
+                                    <label for="name">Nombre</label>
+                                    <input type="text" class="form-control" id="name">
+                                  </div>
                                 </div>
+
+
+                                <div class="col-12 col-md-6 col-lg-6">
+                                   <div class="form-group">
+                                     <label for="reason_social">Razón Social</label>
+                                     <input type="text" class="form-control" id="reason_social">
+                                   </div>
+                                </div>
+
+                                <div class="col-12">
+                                   <div class="form-group">
+                                     <label for="address">Dirección</label>
+                                     <input type="text" class="form-control" id="address">
+                                   </div>
+                                </div>
+
+                                <div class="col-12 pb-4">
+                                   <span class="font-weight-normal">Módulos</span><br> 
+                                   <div class="form-check form-check-inline">
+                                     <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                                     <label class="form-check-label" for="inlineCheckbox1">Biblioteca</label>
+                                    </div>
+
+                                    <div class="form-check form-check-inline">
+                                      <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                                      <label class="form-check-label" for="inlineCheckbox2">Certificados</label>
+                                    </div>
+
+                                   <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" >
+                                        <label class="form-check-label" for="inlineCheckbox3">Virtual</label>
+                                    </div>
+
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">
+                                        <label class="form-check-label" for="inlineCheckbox3">Pago</label>
+                                    </div>
+                                </div>
+
+                            </div>
+
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="toggleModal()">Cerrar</button>
